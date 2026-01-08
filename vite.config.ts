a3108a39f74227ec4ea677a317cb7adb8acf1f73
Rich Buggy <richard@baileynelson.com.au>
2026-01-09 00:00:39 +1100

    build: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,47 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: "eslint .",
+        useFlatConfig: true,
+      },
+    }),
+  ],
+  test: {
+    environment: "jsdom",
+    environmentOptions: {
+      jsdom: {
+        console: false,
+      },
+    },
+    globals: false,
+    testTimeout: 20000,
+    passWithNoTests: true,
+    coverage: {
+      include: ["<rootDir>/app/**"],
+      exclude: ["<rootDir>/node_modules/", "<rootDir>/build", "**/*.d.ts"],
+      thresholds: {
+        global: {
+          branches: 60,
+          functions: 70,
+          lines: 75,
+          statements: 75,
+        },
+      },
+      enabled: true,
+      provider: "v8",
+      reporter: ["json", "html"],
+      reportsDirectory: "./coverage",
+    },
+  },
 });
