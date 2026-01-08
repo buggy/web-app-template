296e05bee898559f8db6e4ec5de39daba79d18ab
Rich Buggy <richard@baileynelson.com.au>
2026-01-09 00:32:14 +1100

    build: add playwright for e2e tests

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,13 +1,19 @@
+import { includeIgnoreFile } from "@eslint/compat";
 import jsPlugin from "@eslint/js";
 import vitestPlugin from "@vitest/eslint-plugin";
 import prettierPlugin from "eslint-config-prettier/flat";
+import playwright from "eslint-plugin-playwright";
 import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
 import testingLibraryPlugin from "eslint-plugin-testing-library";
 import { defineConfig } from "eslint/config";
+import { fileURLToPath } from "node:url";
 import tsPlugin from "typescript-eslint";
 
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
 export default defineConfig([
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
   jsPlugin.configs.recommended,
   tsPlugin.configs.strict,
   tsPlugin.configs.stylistic,
@@ -20,11 +26,25 @@ export default defineConfig([
     },
   },
   reactHooksPlugin.configs.flat["recommended-latest"],
-  vitestPlugin.configs.all,
-  testingLibraryPlugin.configs["flat/react"],
   {
-    ignores: [".react-router/*", "build/*", "package-lock.json"],
+    ...vitestPlugin.configs.all,
+    files: ["test/**.[tj]sx?", "app/**.(spec|test).[tj]sx?"],
   },
+  {
+    ...testingLibraryPlugin.configs["flat/react"],
+    files: ["test/**.[tj]sx?", "app/**.(spec|test).[tj]sx?"],
+  },
+  {
+    ...playwright.configs["flat/recommended"],
+    rules: {
+      ...playwright.configs["flat/recommended"].rules,
+    },
+  },
+
+  // {
+  //   ...playwright.configs["flat/recommended"],
+  //   files: ["e2e"],
+  // },
   {
     rules: {
       "no-console": "error",
